6511f2f84f147bd622533e7ffffe84504e884a2b
hunghung166 <hungnvd00436@fpt.edu.vn>
2018-06-12 09:38:39 +0700

    Add project files.

---
+++ ConsumingRESTServiceCRUD_Client/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ConsumingRESTServiceCRUD_Client.Startup))]
+namespace ConsumingRESTServiceCRUD_Client
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
